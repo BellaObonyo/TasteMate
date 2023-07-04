5c5f42b708f29afb0dafbe276856766ee0bad65d
BellaObonyo <cynthiabella.obonyo@gmail.com>
2023-07-04 12:44:32 +0300

    add welcome page

--- src/components/WelcomePage/welcome.js
+++ src/components/WelcomePage/welcome.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
 
-export default function SplashPage() {
+export default function Welcome() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>TasteMate</Text>
